d9fb8407011eda03a8087a21a29918f90fd13bdb
jthyer <john.thyer@gmail.com>
2024-03-08 09:29:10 -0600

    Figuring out closures.

--- main.lua
+++ main.lua
@@ -6,6 +6,8 @@ global.TILE_DIMENSION = 16
 kb = require("source.kb")
 util = require("source.util")
 
+require("closure")
+
 local asset = require("source.asset")
 local scene = require("source.scene")
 
@@ -28,11 +30,20 @@ function love.update(dt)
     scene.update()
     accumulator = accumulator - tickPeriod
   end  
+  
+  for i,v in ipairs(objects) do
+    v.update()
+  end
 end
 
 function love.draw()
   scene.draw()
   kb.draw()
+
+  for i,v in ipairs(objects) do
+    love.graphics.rectangle("fill",v.get_x(),v.get_y(),32,32)  
+  end
+
 end
 
 function global.getAsset(assetType,index)
--- source/class/wall.lua
+++ source/class/wall.lua
@@ -1,11 +1,16 @@
-local wall = {}
-
-function wall:load()
-  self.visible = false
-end
-
-function wall:update(p)
+-- wall implementation closure
+local function wall(start_x,start_y)
+  local public = {}
   
+  local x = start_x
+  local y = start_y
+  
+  function public.get_x() return x end
+  function public.get_y() return y end
+  function public.update() end
+  
+  return public
 end
 
 return wall
+
